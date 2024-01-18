f8af6d41d46ef80191e8be170d400209096a1d44
Maks <sharikwalks@gmail.com>
2024-01-18 14:36:53 +0300

    Script for converting an image from the img folder to ascii graphics. * added function of saving the finished file by pressing s

--- ascii.py
+++ ascii.py
@@ -1,34 +1,34 @@
 import pygame as pg
 import cv2
-
-
+import os
 class ArtConverter:
-    def __init__(self, path='img/car.jpg', font_size=10):
+    def __init__(self, path='img/car.jpg', font_size=12):
         pg.init()
         self.path = path
         self.image = self.get_image()
-        self.RES = self.WIDTH, self.HEIGHT = self.image.shape[1], self.image.shape[0]
+        self.RES = self.HEIGHT, self.WIDTH = self.image.shape[0], self.image.shape[1]
         self.surface = pg.display.set_mode(self.RES)
         self.clock = pg.time.Clock()
 
-        self.ASCII_CHARS = ' @%#*+=-:.'
-        self.ASCII_COEFF = 255 / (len(self.ASCII_CHARS) - 1)
-
+        self.ASCII_CHARS = " .,:;irs?@$%&#=+-1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.ASCII_COEFFICIENT = 255 // (len(self.ASCII_CHARS) - 1)
         self.font = pg.font.SysFont('Courier', font_size, bold=True)
         self.CHAR_STEP = int(font_size * 0.6)
         self.RENDERED_ASCII_CHARS = [self.font.render(char, False, 'white') for char in self.ASCII_CHARS]
 
     def draw_converted_image(self):
-        char_indices = self.image // self.ASCII_COEFF
-        for y in range(0, self.HEIGHT - self.CHAR_STEP, self.CHAR_STEP):
-            for x in range(0, self.WIDTH - self.CHAR_STEP, self.CHAR_STEP):
-                char_index = int(char_indices[y, x])
-                if char_index:
-                    self.surface.blit(self.RENDERED_ASCII_CHARS[char_index], (x, y))
+        char_indices = self.image // self.ASCII_COEFFICIENT
+        for x in range(0, self.WIDTH, self.CHAR_STEP):
+            for y in range(0, self.HEIGHT, self.CHAR_STEP):
+                char_index = char_indices[y, x]
+                if char_index < len(self.RENDERED_ASCII_CHARS):
+                    char = self.RENDERED_ASCII_CHARS[char_index]
+                    self.surface.blit(char, (y, x))
 
     def get_image(self):
         self.cv2_image = cv2.imread(self.path)
-        gray_image = cv2.cvtColor(self.cv2_image, cv2.COLOR_BGR2GRAY)
+        transposed_image = cv2.transpose(self.cv2_image)
+        gray_image = cv2.cvtColor(transposed_image, cv2.COLOR_BGR2GRAY)
         return gray_image
 
     def draw_cv2_image(self):
@@ -40,26 +40,29 @@ class ArtConverter:
         self.draw_converted_image()
         self.draw_cv2_image()
 
-    def save_image(self):
-        pygame_image = pg.surfarray.array3d(self.surface)
-        cv2_img = cv2.transpose(pygame_image)
-        cv2.imwrite('output\img\converted_ascii_image.png', cv2_img)
-
     def run(self):
         while True:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    pg.quit()
+                    exit()
                 elif event.type == pg.KEYDOWN:
                     if event.key == pg.K_s:
                         self.save_image()
-                      
-                    return
+                        print('Image saved')
             self.draw()
             pg.display.set_caption(str(self.clock.get_fps()))
             pg.display.flip()
             self.clock.tick()
 
+    def save_image(self):
+        pygame_image = pg.surfarray.array3d(self.surface)
+        numpy_image = cv2.cvtColor(pygame_image, cv2.COLOR_RGB2BGR)
+        transposed_image = cv2.transpose(numpy_image)
+
+        output_dir = 'output/img'
+        os.makedirs(output_dir, exist_ok=True)  # Создание папки, если она не существует
+
+        cv2.imwrite(os.path.join(output_dir, 'ascii_image.jpg'), transposed_image)
 
 if __name__ == "__main__":
     app = ArtConverter()
